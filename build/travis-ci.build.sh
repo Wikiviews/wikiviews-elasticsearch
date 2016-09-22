eba3ddbfe23f8a11ee33f076d05576f8e26294c2
DevWurm <devwurm@devwurm.net>
2016-09-22 15:10:46 +0200

    Updating build tag

--- build/travis-ci.build.sh
+++ build/travis-ci.build.sh
@@ -4,7 +4,8 @@ COMMIT=$(echo $TRAVIS_COMMIT | cut -b1-7)
 
 if [ $TRAVIS_TAG ]
 then
-  docker build -f Dockerfile -t "$REPO:$TRAVIS_TAG" -t "$REPO:latest" .
+  TAG=$(echo $TRAVIS_TAG | sed 's/v//g')
+  docker build -f Dockerfile -t "$REPO:$TAG" -t "$REPO:latest" .
 else
   docker build -f Dockerfile -t "$REPO:$COMMIT" .
 fi
